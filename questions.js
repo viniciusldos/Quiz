5bf0a61395c72e41f56087a42bdbb8750f2c7a65
Vinicius Limirio <viniciusldos@gmail.com>
2022-11-23 14:46:14 -0300

    first commit

---
+++ questions.js
@@ -0,0 +1,85 @@
+export default [
+  {
+    question: "Quem criou os personagens Mickey e Minnie Mouse?",
+    answers: [
+      { option: "Walt Disney", correct: true },
+      { option: "Stephen King", correct: false },
+      { option: "J. K. Rowling", correct: false },
+    ],
+  },
+  {
+    question:
+      "Como se chamam os dois melhores amigos do personagem Harry Potter?",
+    answers: [
+      { option: "Salsicha e Scooby Doo", correct: false },
+      { option: "Hermione e Rony", correct: true },
+      { option: "Sam e Dean", correct: false },
+    ],
+  },
+  {
+    question: " Qual é a cor das famosas cabines telefônicas de Londres?",
+    answers: [
+      { option: "Verdes", correct: false },
+      { option: "Amarelas", correct: false },
+      { option: "Vermelhas", correct: true },
+    ],
+  },
+  {
+    question:
+      "Qual é o nome agente secreto da ficção conhecido pelo número 007?",
+    answers: [
+      { option: "Ethan Hunt", correct: false },
+      { option: "Bob Lee", correct: false },
+      { option: "James Bond", correct: true },
+    ],
+  },
+  {
+    question: "Qual é o maior país do mundo?",
+    answers: [
+      { option: "Brasil", correct: false },
+      { option: "Rússia", correct: true },
+      { option: "Estados Unidos", correct: false },
+    ],
+  },
+  {
+    question:
+      "Em qual cidade dos Estados Unidos fica a famosa Estátua da Liberdade?",
+    answers: [
+      { option: "Los Angeles", correct: false },
+      { option: "Nova York", correct: true },
+      { option: "Miami", correct: false },
+    ],
+  },
+  {
+    question: " Em que ano o homem pisou na lua?",
+    answers: [
+      { option: "1959", correct: false },
+      { option: "1969", correct: true },
+      { option: "1979", correct: false },
+    ],
+  },
+  {
+    question: " Em qual temperatura a água ferve?",
+    answers: [
+      { option: "100º C", correct: true },
+      { option: "101º C", correct: false },
+      { option: "99º C", correct: false },
+    ],
+  },
+  {
+    question: "Em que ano terminou a Segunda Guerra Mundial?",
+    answers: [
+      { option: "1948", correct: false },
+      { option: "1944", correct: false },
+      { option: "1945", correct: true },
+    ],
+  },
+  {
+    question: "Qual cantor tem o nome artístico de Nivaldo Batista Lima?",
+    answers: [
+      { option: "Luan Santana", correct: false },
+      { option: "Gusttavo Lima", correct: true },
+      { option: "Felipe Araújo", correct: false },
+    ],
+  },
+];
